b68a3170e1bb18604d861045b5eae8faa06704a3
serhiikorol <korol_sergey@me.com>
2025-03-24 19:59:07 +0200

    added saving file to storage

--- Kommunist.Application/ViewModels/ICalConfigViewModel.cs
+++ Kommunist.Application/ViewModels/ICalConfigViewModel.cs
@@ -1,4 +1,7 @@
+using System.Text;
 using System.Windows.Input;
+using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Storage;
 using Ical.Net.CalendarComponents;
 using Ical.Net.DataTypes;
 using Ical.Net.Serialization;
@@ -17,7 +20,7 @@ public class ICalConfigViewModel : BaseViewModel, IQueryAttributable
 
     public ICalConfigViewModel()
     {
-        GenerateIcalCommand = new Command(GenerateIcal);
+        GenerateIcalCommand = new Command(SaveIcalFile);
     }
 
     public void ApplyQueryAttributes(IDictionary<string, object> query)
@@ -25,9 +28,12 @@ public class ICalConfigViewModel : BaseViewModel, IQueryAttributable
         if (query.ContainsKey("SelectedEvents"))
             Events = query["SelectedEvents"] as List<CalEvent> ?? new List<CalEvent>();
     }
+    
+    
 
-    private async void GenerateIcal()
+    private async void SaveIcalFile()
     {
+        // Generate the iCal content
         var calendar = new Ical.Net.Calendar();
         foreach (var ev in Events)
         {
@@ -35,8 +41,7 @@ public class ICalConfigViewModel : BaseViewModel, IQueryAttributable
             {
                 Trigger = new Ical.Net.DataTypes.Trigger(TimeSpan.FromMinutes(-AlarmMinutes).ToString())
             };
-            
-            
+
             var icalEvent = new CalendarEvent
             {
                 Start = new CalDateTime(ev.DateTime),
@@ -49,20 +54,58 @@ public class ICalConfigViewModel : BaseViewModel, IQueryAttributable
 
         var serializer = new CalendarSerializer();
         var icalString = serializer.SerializeToString(calendar);
-        string eventsFolder;
+        
+        // Convert the iCal string to a stream
+        var stream = new MemoryStream(Encoding.UTF8.GetBytes(icalString));
 
-        eventsFolder = Path.Combine(FileSystem.AppDataDirectory, "Events");
-
-// Ensure the directory exists
-        if (!Directory.Exists(eventsFolder))
+        // Save the file
+        var fileSaverResult = await FileSaver.Default.SaveAsync("events.ics", stream);
+        if (fileSaverResult.IsSuccessful)
         {
-            Directory.CreateDirectory(eventsFolder);
+            await Toast.Make($"The file was saved successfully to location: {fileSaverResult.FilePath}").Show();
         }
-
-        var filePath = Path.Combine(eventsFolder, "events.ics");
-        await File.WriteAllTextAsync(filePath, icalString);
-
-
-        await Shell.Current.DisplayAlert("Success", "iCal file generated!", "OK");
+        else
+        {
+            await Toast.Make($"The file was not saved successfully with error: {fileSaverResult.Exception.Message}").Show();
+        }
     }
+
+    
+    // private async void GenerateIcal()
+    // {
+    //     var calendar = new Ical.Net.Calendar();
+    //     foreach (var ev in Events)
+    //     {
+    //         var alarm = new Alarm
+    //         {
+    //             Trigger = new Ical.Net.DataTypes.Trigger(TimeSpan.FromMinutes(-AlarmMinutes).ToString())
+    //         };
+    //         
+    //         
+    //         var icalEvent = new CalendarEvent
+    //         {
+    //             Start = new CalDateTime(ev.DateTime),
+    //             Summary = ev.Title,
+    //             Description = $"{ev.Description}\n{Notes}"
+    //         };
+    //         icalEvent.Alarms.Add(alarm);
+    //         calendar.Events.Add(icalEvent);
+    //     }
+    //
+    //     var serializer = new CalendarSerializer();
+    //     var icalString = serializer.SerializeToString(calendar);
+    //
+    //     var eventsFolder = Path.Combine(FileSystem.AppDataDirectory, "Events");
+    //
+    //     if (!Directory.Exists(eventsFolder))
+    //     {
+    //         Directory.CreateDirectory(eventsFolder);
+    //     }
+    //
+    //     var filePath = Path.Combine(eventsFolder, "events.ics");
+    //     await File.WriteAllTextAsync(filePath, icalString);
+    //
+    //
+    //     await Shell.Current.DisplayAlert("Success", "iCal file generated!", "OK");
+    // }
 }
