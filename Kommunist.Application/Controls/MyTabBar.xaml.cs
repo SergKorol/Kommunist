d63e019c19c0487b5dae0d5c6f091ff8ac36954a
serhiikorol <korol_sergey@me.com>
2025-04-23 19:48:18 +0300

    redesigned events calendar

--- Kommunist.Application/Controls/MyTabBar.xaml.cs
+++ Kommunist.Application/Controls/MyTabBar.xaml.cs
@@ -1,12 +1,41 @@
+using System.Windows.Input;
+
 namespace Kommunist.Application.Controls;
 
 public partial class MyTabBar : ContentPage
 {
     
+    public static readonly BindableProperty NavigateToCommandProperty =
+        BindableProperty.Create(nameof(NavigateToCommand), typeof(ICommand), typeof(MyTabBar), null);
+
+    public ICommand NavigateToCommand
+    {
+        get => (ICommand)GetValue(NavigateToCommandProperty);
+        set => SetValue(NavigateToCommandProperty, value);
+    }
+    
     public MyTabBar()
     {
         InitializeComponent();
-        BindingContext = this;
+        NavigateToCommand = new Command<string>(OnNavigateTo);
+    }
+    
+    private async void OnNavigateTo(string pageName)
+    {
+        switch (pageName)
+        {
+            case "HomePage":
+                await Shell.Current.GoToAsync("//Home");
+                break;
+            case "FiltersPage":
+                await Shell.Current.GoToAsync("//Filters");
+                break;
+            case "SettingsPage":
+                await Shell.Current.GoToAsync("//Settings");
+                break;
+            default:
+                break;
+        }
     }
     
     private async void OnTabBarNavigation(object sender, TappedEventArgs e)
