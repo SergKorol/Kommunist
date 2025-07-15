1adb97002b166d2762e5f68834fe28d10e8454cb
serhiikorol <korol_sergey@me.com>
2025-07-15 14:23:56 +0300

    added color themes and clean

---
+++ Kommunist.Application/Themes/LightTheme.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kommunist.Application.Themes;
+
+public partial class LightTheme : ResourceDictionary
+{
+    public LightTheme()
+    {
+        InitializeComponent();
+    }
+}
--- Kommunist.Application/Views/SettingsPage.xaml.cs
+++ Kommunist.Application/Views/SettingsPage.xaml.cs
@@ -1,4 +1,6 @@
 using Kommunist.Application.Controls;
+using Kommunist.Application.Enums;
+using Kommunist.Application.Themes;
 
 namespace Kommunist.Application.Views;
 
@@ -7,5 +9,34 @@ public partial class SettingsPage : MyTabBar
     public SettingsPage()
     {
         InitializeComponent();
+    }
+    
+    void OnPickerSelectionChanged(object sender, EventArgs e)
+    {
+        Picker picker = sender as Picker;
+        Theme theme = (Theme)picker.SelectedItem;
+
+        ICollection<ResourceDictionary> mergedDictionaries = Microsoft.Maui.Controls.Application.Current.Resources.MergedDictionaries;
+        if (mergedDictionaries != null)
+        {
+            mergedDictionaries.Clear();
+
+            switch (theme)
+            {
+                case Theme.Dark:
+                    mergedDictionaries.Add(new DarkTheme());
+                    break;
+                case Theme.Light:
+                default:
+                    mergedDictionaries.Add(new LightTheme());
+                    break;
+            }
+            StatusLabel.Text = $"{theme.ToString()} theme loaded. Close this page.";
+        }
+    }
+
+    public async Task Dismiss()
+    {
+        await Navigation.PopModalAsync();
     }
 }
