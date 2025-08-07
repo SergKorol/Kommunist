1749d2bb540616b98bfc018199bb7a2950be912f
serhiikorol <korol_sergey@me.com>
2025-08-07 17:22:03 +0300

    refactor helpers

--- Kommunist.Core/Helpers/ObservableHashSetExtensions.cs
+++ Kommunist.Core/Helpers/ObservableHashSetExtensions.cs
@@ -6,6 +6,8 @@ public static class ObservableHashSetExtensions
 {
     public static ObservableHashSet<T> ToObservableHashSet<T>(this IEnumerable<T> source)
     {
+        ArgumentNullException.ThrowIfNull(source);
+
         var set = new ObservableHashSet<T>();
         foreach (var item in source)
         {
