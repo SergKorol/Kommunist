57196b296e8434a5e113e8b158f7a073a921e553
serhiikorol <korol_sergey@me.com>
2025-08-12 18:32:45 +0300

    refactored themes

--- Kommunist.Application/App.xaml.cs
+++ Kommunist.Application/App.xaml.cs
@@ -4,36 +4,47 @@ namespace Kommunist.Application;
 
 public partial class App 
 {
+    private ResourceDictionary _themeDictionary;
+
     public App()
     {
         InitializeComponent();
         if (Current == null) return;
+
         var savedTheme = Preferences.Get("AppTheme", "Light");
 
         if (Enum.TryParse(savedTheme, out AppTheme userTheme))
         {
             Current.UserAppTheme = userTheme;
         }
-        SetAppThemeResources(Current.RequestedTheme);
 
+        SetAppThemeResources(Current.RequestedTheme);
         Current.RequestedThemeChanged += OnRequestedThemeChanged;
     }
-    
+
     protected override Window CreateWindow(IActivationState activationState)
     {
         return new Window(new AppShell());
     }
-    
+
     private void OnRequestedThemeChanged(object sender, AppThemeChangedEventArgs e)
     {
+        Preferences.Set("AppTheme", e.RequestedTheme.ToString());
         SetAppThemeResources(e.RequestedTheme);
     }
 
     private void SetAppThemeResources(AppTheme theme)
     {
-        if (theme == AppTheme.Dark)
-            Resources.MergedDictionaries.Add(new DarkTheme());
-        else
-            Resources.MergedDictionaries.Add(new LightTheme());
+        if (_themeDictionary is not null)
+        {
+            Resources.MergedDictionaries.Remove(_themeDictionary);
+            _themeDictionary = null;
+        }
+
+        _themeDictionary = theme == AppTheme.Dark
+            ? new DarkTheme()
+            : new LightTheme();
+
+        Resources.MergedDictionaries.Add(_themeDictionary);
     }
 }
