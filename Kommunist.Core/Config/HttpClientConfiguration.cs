6c9b72e97c44d5f80f6b196a971e1b4162005f7a
serhiikorol <korol_sergey@me.com>
2025-08-12 14:52:12 +0300

    refactor config

--- Kommunist.Core/Config/HttpClientConfiguration.cs
+++ Kommunist.Core/Config/HttpClientConfiguration.cs
@@ -1,5 +1,6 @@
 using Kommunist.Core.Services;
 using Kommunist.Core.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
 
 namespace Kommunist.Core.Config;
 
@@ -7,8 +8,20 @@ public static class HttpClientConfiguration
 {
     public static IServiceCollection AddHttpClientConfiguration(this IServiceCollection services)
     {
-        services.AddHttpClient<IEventService, EventService>(client => client.BaseAddress = new Uri("https://wearecommunity.io"));
-        services.AddHttpClient<ISearchService, SearchService>(client => client.BaseAddress = new Uri("https://wearecommunity.io"));
+        services.AddHttpClient<IEventService, EventService>((sp, client) =>
+        {
+            var config = sp.GetRequiredService<IConfiguration>();
+            var baseUrl = config["ApiBaseUrl"] ?? "https://wearecommunity.io";
+            client.BaseAddress = new Uri(baseUrl);
+        });
+
+        services.AddHttpClient<ISearchService, SearchService>((sp, client) =>
+        {
+            var config = sp.GetRequiredService<IConfiguration>();
+            var baseUrl = config["ApiBaseUrl"] ?? "https://wearecommunity.io";
+            client.BaseAddress = new Uri(baseUrl);
+        });
+
         services.AddSingleton<IFileHostingService, FileHostingService>();
         services.AddSingleton<IEmailService, EmailService>();
         services.AddSingleton<IFilterService, FilterService>();
