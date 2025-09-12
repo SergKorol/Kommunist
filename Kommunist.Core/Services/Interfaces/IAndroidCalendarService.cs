d85708f3b4b90b2ff5a5085671d277ded6f158bb
Serhii Korol <korol_sergey@me.com>
2025-09-12 09:51:51 +0300

    refactor: remove unused services, helpers, and models
    - Removed `CoordinatesService` and its interface as they are no longer needed.
    - Deleted `AndroidCalendarService` and its interface after refactoring calendar handling.
    - Removed `CountryExtensions` and `ServiceHelper` due to redundancy.
    - Deleted `NominatimResult` model as it is no longer in use.
    - Updated related resources and dependencies.

--- Kommunist.Core/Services/Interfaces/IAndroidCalendarService.cs
+++
@@ -1,8 +0,0 @@
-namespace Kommunist.Core.Services.Interfaces;
-
-public interface IAndroidCalendarService
-{
-    Task AddEvents(string icsPath, string targetCalendarName = null);
-    Task<string[]> GetCalendarNames();
-
-}
--- Kommunist.Core/Services/Interfaces/ICoordinatesService.cs
+++
@@ -1,6 +0,0 @@
-namespace Kommunist.Core.Services.Interfaces;
-
-public interface ICoordinatesService
-{
-    Task<(double Latitude, double Longitude)> GetCoordinatesAsync(string location);
-}
