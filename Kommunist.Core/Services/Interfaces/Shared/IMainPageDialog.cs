eff3ddd92fa7c8dd9c6f38d44c44195b42c1c022
Serhii Korol <korol_sergey@me.com>
2025-09-25 10:19:30 +0300

    chore: update method signatures to support nullable parameters across `DialogService` and `CalendarService` for consistency and improved null safety

--- Kommunist.Core/Services/Interfaces/Shared/IMainPageDialog.cs
+++ Kommunist.Core/Services/Interfaces/Shared/IMainPageDialog.cs
@@ -2,5 +2,5 @@ namespace Kommunist.Core.Services.Interfaces.Shared;
 
 public interface IMainPageDialog
 {
-    Task<string?> DisplayActionSheet(string title, string cancel, string destruction, string[] buttons);
+    Task<string?> DisplayActionSheet(string title, string cancel, string? destruction, string[] buttons);
 }
--- Kommunist.Core/Services/Interfaces/Shared/IPageDialogService.cs
+++ Kommunist.Core/Services/Interfaces/Shared/IPageDialogService.cs
@@ -1,5 +1,5 @@
 namespace Kommunist.Core.Services.Interfaces.Shared;
 public interface IPageDialogService
 {
-    Task<string?> DisplayActionSheet(string title, string cancel, string destruction, string[] buttons);
+    Task<string?> DisplayActionSheet(string title, string cancel, string? destruction, string[] buttons);
 }
